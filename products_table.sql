cea8e2a2878ca8fb9cc5581a4d1c9f5f6f0bba2c
zibrahim2046 <zibrahim2046@gmail.com>
2020-04-23 11:09:00 -0500

    created bamazonCustomer js file and create sql file to populate products db

---
+++ products_table.sql
@@ -0,0 +1,10 @@
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (1,'Legal Pad','Office',59,6.99);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (2,'55\" Roku TV','Electronics',32,299.99);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (3,'Macbook Pro 13\"','Electronics',208,999.99);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (4,'NIKE Men\'s Shoes','Footwear',25,52.99);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (5,'Women\'s Dress','Clothing',54,15.99);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (6,'ASICS Women\'s Shoes','Footwear',83,89.95);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (7,'6 Drawer Dresser','Furniture',14,99.99);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (8,'Classic Leather Office Chair','Office',4,67.99);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (9,'Hammock and Stand','Outdoor',59,89.99);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (10,'45lb Bumper Plates','Sports',103,189.99);
