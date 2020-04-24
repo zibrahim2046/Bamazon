b8dae9c4c201f54b8d6fdc452745307f6d506462
zibrahim2046 <zibrahim2046@gmail.com>
2020-04-23 22:37:26 -0500

    added function that uses inquirer prompt to take selection and query to bamazon db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,7 +11,53 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your Password
-    password: "password",
+    password: "M@rch201995",
     database: "bamazon"
 
 });
+connection.connect(function(err) {
+    if (err) throw err;
+    showItems();
+  });
+
+  function showItems() {
+      var query = "SELECT * FROM bamazon.products";
+      connection.query(query, function(err, res){
+          console.table(res);
+          qAndA();
+      });
+  }
+
+  function qAndA() {
+      inquirer.prompt([
+          {
+              type: "list",
+              name: "whatToBuy",
+              message: "Please select the ID number of the item you would like to purchase",
+              choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+          },
+          {
+              type: "input",
+              name: "howMany",
+              message: "How many of this item would you like to purchase?"
+          }
+      ])
+      .then(function(response) {
+    var query = "SELECT stock_quantity FROM products where id= '"+
+    response.whatToBuy + "'";
+    connection.query(query, function(err, res) {
+        var dbResponse = res[0].stock_quantity;
+        var requested = JSON.parse(response.howMany);
+        if (request <= dbResponse) {
+            var query = "UPDATE products SET stock_quantity = stock_quantity -" +
+            requested +
+            " WHERE id = '" +
+            response.whatToBuy +
+            "'";
+connection.query()
+
+        }
+    }
+    }
+  }
+
